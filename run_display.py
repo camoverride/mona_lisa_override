bb2f366ebc48e70b814336de5c8bf836967fdb0c
camoverride <camoverride@gmail.com>
2025-03-14 13:20:21 -0700

    force wayland on ubuntu

--- run_display.py
+++ run_display.py
@@ -123,19 +123,19 @@ if __name__ == "__main__":
         # Check if the display server is Xorg or Wayland
         display_server = os.environ.get("XDG_SESSION_TYPE", "").lower()
 
-        if display_server == "x11":  # Xorg
-            print("Ubuntu: using x11")
-            cv2.namedWindow("Display Image", cv2.WND_PROP_FULLSCREEN)
-            cv2.setWindowProperty("Display Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        elif display_server == "wayland":  # Wayland
-            print("Ubuntu: using wayland")
-            # Workaround for Wayland: Manually resize the window to fill the screen
-            cv2.namedWindow("Display Image", cv2.WINDOW_NORMAL)
-            cv2.setWindowProperty("Display Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        else:
-            print("Ubuntu: fallback to defaults!")
-            # Fallback for unknown display servers
-            cv2.namedWindow("Display Image", cv2.WINDOW_NORMAL)
+        # if display_server == "x11":  # Xorg
+        #     print("Ubuntu: using x11")
+        #     cv2.namedWindow("Display Image", cv2.WND_PROP_FULLSCREEN)
+        #     cv2.setWindowProperty("Display Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        # elif display_server == "wayland":  # Wayland
+        print("Ubuntu: using wayland")
+        # Workaround for Wayland: Manually resize the window to fill the screen
+        cv2.namedWindow("Display Image", cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("Display Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        # else:
+        #     print("Ubuntu: fallback to defaults!")
+        #     # Fallback for unknown display servers
+        #     cv2.namedWindow("Display Image", cv2.WINDOW_NORMAL)
 
     cv2.imshow("Display Image", background_image)
     cv2.waitKey(10)
